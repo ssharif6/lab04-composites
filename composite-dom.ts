333d12c7aa4033d0f60a1bc72c17ae7442e5515c
Shaheen Sharifian <shaheen.sharifian@gmail.com>
2017-01-25 10:58:31 -0800

    First commit

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,20 @@ interface IDomElement {
   print();
 }
 
+class TextNode implements IDomElement {
+  print() {
+    console.log("I'm a leaf");
+  }
+}
+
+class DomElement implements IDomElement {
+  
+  private list : IDomElement[] = [];
+
+  print() {
+    this.list.forEach((elem : IDomElement) => {
+        console.log(elem);
+        elem.print();
+    });
+  }
+}
